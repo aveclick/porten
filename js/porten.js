e3f9980a8768548e42b3d47966ef819781ff3878
avamay <avamay481516@gmail.com>
2021-08-13 02:26:25 +0300

    new

--- js/porten.js
+++ js/porten.js
@@ -3,4 +3,8 @@ $(function(){
   $('.header__burger, .header_menu_list').toggleClass('active');
   $('body').toggleClass('lock');
   });
+  $('.header_menu_list a').click(function(event){
+    $('.header__burger, .header_menu_list').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
 });
